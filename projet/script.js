d1ab47ae76e912fa1b1c2662b16d95039df4f4aa
andreanneboisjoli <36456003+andreanneboisjoli@users.noreply.github.com>
2021-02-21 22:53:42 -0500

    mises à jour 21 fev
    lien accueil / liens non soulignés  / carrés de changement de page / couleurs / lien mars /

--- projet/script.js
+++ projet/script.js
@@ -5,18 +5,18 @@
 
 
 
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
+//var video = document.getElementById("myVideo");
+//var btn = document.getElementById("myBtn");
 
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
+//function myFunction() {
+ // if (video.paused) {
+ ////   video.play();
+  //  btn.innerHTML = "Pause";
+  //} else {
+ //   video.pause();
+  //  btn.innerHTML = "Play";
+ // }
+//}
 
 
 
